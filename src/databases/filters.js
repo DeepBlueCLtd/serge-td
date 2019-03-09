4ed14d25f5773877968057f7d23af097aee92f25
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-03-08 22:43:39 -0800

    filter/view checkers listener autoupdates

--- src/databases/filters.js
+++ src/databases/filters.js
@@ -18,31 +18,12 @@ export const filters = {
 export const prefix = 'filter'
 
 // query couchdb filters
-export const changes = (db, filter, f, params) => {
-  const namespace = filter.split('/')[0]
-
-  db.changes({
+export const changes = (db, filter, params) => {
+  return db.changes({
     since: 'now',
     live: true,
     include_docs: true,
     filter: prefix + filter
-  }).on('change', () => {f()}).catch(err => {
-
-    if (!filters[namespace]) throw new Error('filter ' + namespace + ' is not defined.')
-
-    // if filter doesn't exist, create it, and try again
-    if(err.status === 404) {
-      db.put({
-        _id: '_design/' + prefix + namespace,
-        filters: filters[namespace]
-      }).then(() => (changes(db, filter, f, params)))
-    }
-
-    if(err.status === 409) {
-      setTimeout(() => {
-        changes(db, filter, f, params)
-      })
-    }
   })
 }
 
