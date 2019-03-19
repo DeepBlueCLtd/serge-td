e6c04e8b4c4637a2a6615f6ba0f16aa92c11ab71
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-03-19 15:34:11 +0400

    cloned databases list and pouchdb database to see those in realtime. filter for clone database without deleted messages

--- client/src/components/control/rewindList.js
+++ client/src/components/control/rewindList.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import {CardBody, ListGroup, ListGroupItem } from 'reactstrap'
 
-class RewindList extends Component {
-  render() {
-    return(<div/>)
-  }
+import { css } from 'aphrodite/no-important'
+import styles from './styles'
+
+const RewindList = ({ rewinds, removeRewindPoint, selectRewindPoint }) => {
+
+  return (
+    <CardBody className={css(!rewinds.length && styles.hide)}>
+      <p>Rewind Points:</p>
+      <ListGroup>
+        {rewinds.map(({doc}, key) => (
+          <ListGroupItem
+            key={key}
+            tag="button"
+            active={doc.active}
+            onClick={() => selectRewindPoint(doc._id)}
+          >
+            {doc.name}
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    </CardBody>
+  )
 }
 
 RewindList.propTypes = {
   removeRewindPoint: PropTypes.func.isRequired,
   selectRewindPoint: PropTypes.func.isRequired,
-  updateRewindPoints: PropTypes.func.isRequired,
   rewinds: PropTypes.array.isRequired
 }
 
