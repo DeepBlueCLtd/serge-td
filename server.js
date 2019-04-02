d176b0547aec86f2a984b8657f96c22deb7960ec
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-04-02 16:46:22 +0400

    remove leveldown

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
-const PouchDB = require('pouchdb').defaults({
-  prefix: './db/'
-})
+const PouchDB = require('pouchdb-core')
+  .plugin(require('pouchdb-adapter-node-websql'))
+  .defaults({ adapter: 'websql' })
+
+var db = new PouchDB('db/mydb', {adapter: 'websql'});
 const cors = require('cors')
 
 const app = express()
