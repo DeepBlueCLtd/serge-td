e70a878f0711c343519071287de56de35cad5851
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-04-04 09:59:42 +0400

    sqllite3-offline

--- offline/install-modules.js
+++ offline/install-modules.js
@@ -13,7 +13,7 @@ Walk("./node_modules_frontend", "../client/node_modules", false, function(error)
     console.log('frontend modules successfully inistalled.');
     console.log('-------------------------------------------------------------');
   }
-  
+
   console.log('-------------------------------------------------------------');
   console.log('inistalling backend modules...');
   console.log('-------------------------------------------------------------');
@@ -24,8 +24,23 @@ Walk("./node_modules_frontend", "../client/node_modules", false, function(error)
     }
     else {
       console.log('-------------------------------------------------------------');
-      console.log('frontend backend successfully inistalled.');
+      console.log('backend modules successfully inistalled.');
       console.log('-------------------------------------------------------------');
     }
+
+    console.log('-------------------------------------------------------------');
+    console.log('inistalling backend special changed modules for offline...');
+    console.log('-------------------------------------------------------------');
+
+    Walk("./node_modules_special", "../node_modules", false, function(error) {
+      if (error) {
+        throw error;
+      }
+      else {
+        console.log('-------------------------------------------------------------');
+        console.log('backend special changed modules for offline successfully inistalled.');
+        console.log('-------------------------------------------------------------');
+      }
+    });
   });
 });
